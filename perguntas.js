2f95dc4581e7363fb91da55a3adabee2c34aeebe
mellop <142810065+mellop013@users.noreply.github.com>
2024-09-20 11:43:40 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,48 @@
+criaCartao(
+    'Playstation',
+    'Qual foi o jogo mais vendido Playstation?',
+    'Gran Turismo '
+)
+
+criaCartao(
+    'Playstation',
+    'Qual foi o jogo mais vendido do PS5?',
+    'Marvel Spider-Man 2'
+)
+
+criaCartao(
+    'Playstation',
+    'Em que ano a Playstation foi criada?',
+    '3 de dezembro de 1994'
+)
+
+criaCartao(
+    'XBOX',
+    'qual foi o jogo mais vendido do XBOX?',
+    'Minecraft: Xbox One Edition'
+)
+criaCartao(
+    'Microsoft',
+    'Em qual ano foi criada a Microsoft?',
+    '4 de abril de 1975'
+)
+criaCartao(
+    'Microsoft',
+    'Quem é o dono da Microsoft?',
+    'Bill Gatess'
+)
+criaCartao(
+    'Nintendo',
+    'qual foi o jogo mais vendido da nintendo?',
+    'Mario Kart 8'
+)
+criaCartao(
+    'Nintendo',
+    'em que ano a nintendo foi criada?',
+    '23 de setembro de 1889'
+)
+criaCartao(
+    'Nintendo',
+    'qual foi o jogo mais vendido do mundo?',
+    'Minecraft'
+)
